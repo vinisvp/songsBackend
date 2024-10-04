abb3407eac317a4fd920d23245be053dcf0a636c
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-10-04 18:41:47 -0300

    Revert "Made all the Endpoints"
    This reverts commit c7a59e64c1aac761fb72cff5feab073c60c8460b.

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('Hongkongaton', 'D-Sides', '3:35', 'Gorillaz', 'Indie, Dance, Pop');
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('Redbone', '"Awaken, My Love!"', '5:26', 'Childish Gambino', 'R&B, Soul');
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('Run Boy Run', 'The Golden Age', '3:33', 'Woodkid', 'Chamber pop, Neofolk, Martial industrial');
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('Legendary', 'Ruins', '2:50', 'Black Math', 'Indie, pop');
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('In Too Deep', 'Texture', '3:21', 'Battle Tapes', 'Dance, Eletrônica');
-INSERT INTO TBL_SONGS (NAME, ALBUM, LENGTH, ARTIST, GENRE) VALUES ('Uebok', 'Renaissance', '2:43', 'Apashe, INSTASAMKA', 'Dance, Eletrônica, Trap');
